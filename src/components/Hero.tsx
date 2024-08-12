26664eb929d43a780930b06c8ff034208da6c381
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2024-08-12 12:19:34 -0600

    added hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,33 @@
+import Fab from '@mui/material/Fab';
+import { styled } from '@mui/system';
+
+const FabButton = styled(Fab)({
+    position: 'fixed',
+    bottom: '2rem',
+    right: '2rem',
+    zIndex: 2000,
+    backgroundColor: 'var(--accent-color)',
+    color: 'white', '&:hover' :{
+        backgroundColor: 'var(--accent-color-dark)'
+    },
+  });
+
+function Hero() {
+    return (
+        <>
+            <div className="hero-container">
+                <video className="hero-video" autoPlay loop muted>
+                    <source src="https://storage.googleapis.com/msgsndr/LXS2IKeUcdo35swibTLp/media/668b35984a1876758620ac6f.mp4" type="video/mp4" />
+                    
+                </video>
+                <h1>Move Your Patients</h1>
+                <h2>Safely, Confidently, and Efficiently</h2>
+            </div>
+            <FabButton variant="extended" color="primary">
+                Free Consultation!
+            </FabButton>
+        </>
+    );
+}
+
+export default Hero;
