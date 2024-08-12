24aebe6dad8a25683625c97aac2b4d759af963bf
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2024-08-12 12:09:34 -0600

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import NursifyServices from './components/NursifyServices'
+import ExperienceButtons from './components/ExperienceButtons'
+import InteractiveGrid from './components/InteractiveGrid'
+import NursifyTeam from './components/NursifyTeam'
+import ModuleSlides from './components/ModuleSlides'
+import ScrollingReviews from './components/ScrollingReviews'
+import NursifyCertification from './components/NursifyCertification'
+import Footer from './components/Footer'
+
+import './styles/App.css'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedCategory, setSelectedCategory] = useState<string>('University Administrations');
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar/>
+      <Hero />
+      <NursifyServices />
+      <ExperienceButtons onChange={setSelectedCategory}/>
+      <InteractiveGrid category={selectedCategory}/>
+      <NursifyTeam />
+      <ModuleSlides />
+      <NursifyCertification/>
+      <ScrollingReviews />
+      <Footer />
     </>
   )
 }
