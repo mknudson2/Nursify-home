c407fe9916608e56e8e8b7c18a17a8c5a1ade6da
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2024-08-12 12:13:29 -0600

    added Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import { IconContext } from "react-icons";
+import { FaInstagram, FaYoutube, FaTiktok, FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <>
+        <section className="footer-whole">
+            <div className="footer-container">
+                <p>&copy; Copyright Nursify Eduction, 2024</p>
+                <div className="social-media">
+                    <IconContext.Provider value={{ size: "25px", color: "var(--accent-color)" }}>
+                        <FaFacebook />
+                        <FaInstagram />
+                        <FaTiktok />
+                        <FaXTwitter />
+                        <FaYoutube />
+                    </ IconContext.Provider>
+                </div>
+                <div className="legal-links">
+                    <p>Terms of Use</p>
+                    <p>Privacy Policy</p>
+                </div>
+
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Footer;
