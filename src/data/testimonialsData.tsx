64a7da128c303b9febadfa87154c12886dff558c
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2024-08-21 22:42:55 -0600

    made changes to testimonials

--- src/data/testimonialsData.tsx
+++ src/data/testimonialsData.tsx
@@ -12,142 +12,143 @@ export interface Testimonial {
   export const testimonials: Testimonial[] = [
     {
       id: 1,
-      name: 'Orson Krennic',
-      title: 'Director',
-      image: '/krennic.jpg',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Excellent!"',
+      name: 'Kimberly Bogers',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: 'My CNA students told me that they feel more motivated, more inspired, and less scared.',
+      keyphrase: '"Empowering!"',
       rating: 5,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'After receiving your training, my CNA students told me that they feel more motivated, more inspired, and less scared about transferring and transporting their patients and residents. They feel empowered and confident about their abilities to keep their patients/residents safe and to get them where they need to go. Several of them told me that your guest lectures have been their favorite part of the semester so far.'
     },
     {
       id: 2,
-      name: 'Halldor Hrafnkelsson',
-      title: 'Doctor',
-      image: '/5.png',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Well worth it!"',
+      name: 'Student',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: 'It made transferring people so much easier.',
+      keyphrase: 'Super fun',
       rating: 4,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'I loved having the guest speakers, they were super fun. Phil was especially beneficial, as it made transferring people so much easier.'
     },
     {
       id: 3,
-      name: 'Þorgeir Berharðsson',
-      title: 'PT',
-      image: '/1.png',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Superb!"',
+      name: '3rd Year Student',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: 'He really pushes us to think outside of what is traditionally known.',
+      keyphrase: '"Amazing Quality!"',
       rating: 4,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: "I'm going to come and I am becoming he pushes you to think outside of the box while also creating an environment that's really conducive to your learning style specifically and so he's very flexible and adaptable to what you need which I think is an amazing quality to have as someone who's a mentor along with that he also really pushes us to think outside of which traditionally known and challenges those Concepts."
     },
     {
       id: 4,
-      name: 'Jóanna Egilsdóttir',
-      title: 'NP',
-      image: '/deathTrooper.avif',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Exactly What We Needed"',
+      name: 'Practicing Therapist',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: ' I am excited to implement these transfers.',
+      keyphrase: '"Definitely Recommend!"',
       rating: 3,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'I would definitely recommend this course to any rehab professional that wants a refresher on transfers.  That wants to learn new techniques that are safer and easier for yourself and the patient and how to adapt these transfers to patients of various diagnoses.  I am excited to implement these transfers.'
     },
     {
       id: 5,
-      name: 'Sigmund Rafnsson',
-      title: 'OT',
-      image: '/crosshair.webp',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Frábært!"',
+      name: 'OT',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: 'I would highly recommend taking this course.',
+      keyphrase: '"Really Impressed."',
       rating: 5,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'I was really impressed with how it went and He did a really good job of covering the foundational aspects of transfers including principles like weight acceptance tibial translation.  Also, implemented and showed us quite a few Condition specific transfers relating to low seated transfer, kneeling transfer, and standing transfers. I would highly recommend taking this course if you are at all not comfortable with transfers or just want to be that much better at them.'
     },
     {
       id: 6,
-      name: 'Hárekr Héðinsson',
-      title: 'Wizard',
-      image: '/revan.jpeg',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Superb!"',
+      name: '16 Year Therapist',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: 'It has really helped me to gain confidence in trying to implement more and more of these principles.',
+      keyphrase: '"More confidence!"',
       rating: 4,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'It has really helped me to gain confidence in trying to implement more and more of these principles.  I am happy to say that I feel like I am doing it more and more without even thinking about it.  I just look forward to ongoing discussions on how to keep learning.'
     },
     {
       id: 7,
-      name: 'Thrawn',
-      title: 'Grand Admiral',
-      image: '/thrawn.jpg',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Beyond Praise"',
+      name: 'Carolyn Lewis',
+      title: 'PhD, RN, CNE',
+      image: './smallLogo.jpeg',
+      review: 'Your presentation was professional, fun, friendly, and absolutely engaging.',
+      keyphrase: '"Wow!"',
       rating: 5,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'Wow.  You had everyone interested, learning, and asking questions.  You expounded very effectively the role of the OT and demonstrated transfer skills, adaptation, and how the nurse could definitely be more efficient by learning some of the patient transfer skills that OT knows. When your presentation time was up, we wanted and needed more.  Phil your presentation was professional, fun, friendly, and absolutely engaging.  So good, in fact, that I would love to involve your department each semester for the IPE activity in the 4th  semester, if possible, or, at least, each Spring semester.  Is this something we could plan for? Please know how much we appreciate you taking the time to help me with IPE. It was a synergistic and beneficial collaboration between nursing and occupational therapy for sure. Thank you again for sharing your expertise and supporting a fabulous IPE learning experience for our students.'
     },
     {
       id: 8,
-      name: 'Orson Krennic',
-      title: 'Director',
-      image: '/krennic.jpg',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Excellent!"',
+      name: 'Kimberly Bogers',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: 'My CNA students told me that they feel more motivated, more inspired, and less scared.',
+      keyphrase: '"Empowering!"',
       rating: 5,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'After receiving your training, my CNA students told me that they feel more motivated, more inspired, and less scared about transferring and transporting their patients and residents. They feel empowered and confident about their abilities to keep their patients/residents safe and to get them where they need to go. Several of them told me that your guest lectures have been their favorite part of the semester so far.'
+  
     },
     {
       id: 9,
-      name: 'Halldor Hrafnkelsson',
-      title: 'Doctor',
-      image: '/5.png',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Well worth it!"',
+      name: 'Student',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: 'It made transferring people so much easier.',
+      keyphrase: 'Super fun',
       rating: 4,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'I loved having the guest speakers, they were super fun. Phil was especially beneficial, as it made transferring people so much easier.'
     },
     {
       id: 10,
-      name: 'Þorgeir Berharðsson',
-      title: 'PT',
-      image: '/1.png',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Superb!"',
+      name: '3rd Year Student',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: 'He really pushes us to think outside of what is traditionally known.',
+      keyphrase: '"Amazing Quality!"',
       rating: 4,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: "I'm going to come and I am becoming he pushes you to think outside of the box while also creating an environment that's really conducive to your learning style specifically and so he's very flexible and adaptable to what you need which I think is an amazing quality to have as someone who's a mentor along with that he also really pushes us to think outside of which traditionally known and challenges those Concepts."
     },
     {
       id: 11,
-      name: 'Jóanna Egilsdóttir',
-      title: 'NP',
-      image: '/deathTrooper.avif',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Exactly What We Needed"',
+      name: 'Practicing Therapist',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: ' I am excited to implement these transfers.',
+      keyphrase: '"Definitely Recommend!"',
       rating: 3,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'I would definitely recommend this course to any rehab professional that wants a refresher on transfers.  That wants to learn new techniques that are safer and easier for yourself and the patient and how to adapt these transfers to patients of various diagnoses.  I am excited to implement these transfers.'
     },
     {
       id: 12,
-      name: 'Sigmund Rafnsson',
-      title: 'OT',
-      image: '/crosshair.webp',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Frábært!"',
+      name: 'OT',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: 'I would highly recommend taking this course.',
+      keyphrase: '"Really Impressed."',
       rating: 5,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'I was really impressed with how it went and He did a really good job of covering the foundational aspects of transfers including principles like weight acceptance tibial translation.  Also, implemented and showed us quite a few Condition specific transfers relating to low seated transfer, kneeling transfer, and standing transfers. I would highly recommend taking this course if you are at all not comfortable with transfers or just want to be that much better at them.'
     },
     {
       id: 13,
-      name: 'Hárekr Héðinsson',
-      title: 'Wizard',
-      image: '/revan.jpeg',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Superb!"',
+      name: '16 Year Therapist',
+      title: '',
+      image: './smallLogo.jpeg',
+      review: 'It has really helped me to gain confidence in trying to implement more and more of these principles.',
+      keyphrase: '"More confidence!"',
       rating: 4,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'It has really helped me to gain confidence in trying to implement more and more of these principles.  I am happy to say that I feel like I am doing it more and more without even thinking about it.  I just look forward to ongoing discussions on how to keep learning.'
     },
     {
       id: 14,
-      name: 'Thrawn',
-      title: 'Grand Admiral',
-      image: '/thrawn.jpg',
-      review: 'Nursify has transformed the way I work. The techniques are invaluable.',
-      keyphrase: '"Beyond Praise"',
+      name: 'Carolyn Lewis',
+      title: 'PhD, RN, CNE',
+      image: './smallLogo.jpeg',
+      review: 'Your presentation was professional, fun, friendly, and absolutely engaging.',
+      keyphrase: '"Wow!"',
       rating: 5,
-      fullReview: 'Nursify has transformed the way I work. The techniques are invaluable. I have been able to save so much time and reduce my risk of injury thanks to the expert guidance provided by Nursify.'
+      fullReview: 'Wow.  You had everyone interested, learning, and asking questions.  You expounded very effectively the role of the OT and demonstrated transfer skills, adaptation, and how the nurse could definitely be more efficient by learning some of the patient transfer skills that OT knows. When your presentation time was up, we wanted and needed more.  Phil your presentation was professional, fun, friendly, and absolutely engaging.  So good, in fact, that I would love to involve your department each semester for the IPE activity in the 4th  semester, if possible, or, at least, each Spring semester.  Is this something we could plan for? Please know how much we appreciate you taking the time to help me with IPE. It was a synergistic and beneficial collaboration between nursing and occupational therapy for sure. Thank you again for sharing your expertise and supporting a fabulous IPE learning experience for our students.'
     }
   ];
